6400df3fe26a7bf8fe003f42601a8b8f83848c74
Matt <mmaguire333@gmail.com>
2023-02-06 14:02:04 -0700

    create function for getting latitude and longitude of a given city

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log();
+function getLatLong(city) {
+  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=45e75944bb8affc837985a8360bb4a02`, { mode: 'cors' })
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch(() => {
+      // FIXME: change alert to error message that appears under search box
+      alert('city not found');
+    });
+}
