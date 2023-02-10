975fe4ba7ffa2d7ec820008811f7b281786d305d
Matt <mmaguire333@gmail.com>
2023-02-10 15:51:21 -0700

    Finish styling and displaying data on page

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+function toTwelveHourTime(timeString) {
+  const hour = Number(timeString.substring(0, 2));
+  if (hour > 0 && hour < 12) {
+    return `${hour}AM`;
+  } if (hour === 0) {
+    return '12AM';
+  } if (hour === 12) {
+    return '12PM';
+  }
+  return `${hour - 12}PM`;
+}
+
+function capitalizeDescription(str) {
+  const arr = str.split(' ');
+  const newArr = new Array(arr.length);
+  for (let i = 0; i < arr.length; i += 1) {
+    newArr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  return newArr.join(' ');
+}
+
 async function getLatLong(city) {
   const latLonArray = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=45e75944bb8affc837985a8360bb4a02`, { mode: 'cors' })
     .then((response) => response.json())
     .then((response) => [response[0].lat, response[0].lon])
-    .catch(() => {
+    .catch((error) => {
+      console.log(error);
       document.querySelector('.error').style.display = 'flex';
     });
   return latLonArray;
@@ -15,13 +37,14 @@ async function getCurrentWeatherData(city) {
     .then((currentData) => ({
       temp: currentData.main.temp,
       feelsLike: currentData.main.feels_like,
-      description: currentData.weather[0].description,
+      description: capitalizeDescription(currentData.weather[0].description),
       iconCode: currentData.weather[0].icon,
       windSpeed: currentData.wind.speed,
       windDirection: currentData.wind.deg,
       city: currentData.name,
     }))
-    .catch(() => {
+    .catch((error) => {
+      console.log(error);
       document.querySelector('.error').style.display = 'flex';
     });
   return currentWeather;
@@ -32,7 +55,8 @@ async function getFiveDayForecastData(city) {
   const forecastArray = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latLon[0]}&lon=${latLon[1]}&appid=45e75944bb8affc837985a8360bb4a02&units=imperial`, { mode: 'cors' })
     .then((response) => response.json())
     .then((data) => [data.list, data.city])
-    .catch(() => {
+    .catch((error) => {
+      console.log(error);
       document.querySelector('.error').style.display = 'flex';
     });
   const dataArray = [];
@@ -48,20 +72,18 @@ async function getFiveDayForecastData(city) {
       temp: forecastArray[0][i].main.temp,
       feelsLike: forecastArray[0][i].main.feels_like,
       probabilityOfPrecip: forecastArray[0][i].pop,
-      description: forecastArray[0][i].weather[0].description,
+      description: capitalizeDescription(forecastArray[0][i].weather[0].description),
       iconCode: forecastArray[0][i].weather[0].icon,
     });
   }
   return dataArray;
 }
 
-// display weather data on submitting city
 const searchForm = document.querySelector('.search-form');
 const forecastContainer = document.querySelector('.five-day-forecast');
 
-searchForm.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  const searchValue = document.getElementById('search').value;
+async function displayPage(valueOfSearch) {
+  const searchValue = valueOfSearch;
   const currentWeather = await getCurrentWeatherData(searchValue);
   const forecast = await getFiveDayForecastData(searchValue);
 
@@ -94,7 +116,7 @@ searchForm.addEventListener('submit', async (e) => {
     canonicalDirection = '';
   }
 
-  document.querySelector('.wind').textContent = `Wind: ${currentWeather.windSpeed} mph ${canonicalDirection}`;
+  document.querySelector('.wind').textContent = `Wind: ${Math.round(currentWeather.windSpeed)} mph ${canonicalDirection}`;
   document.querySelector('.description').textContent = currentWeather.description;
   document.getElementById('current-description-image').src = `./Icons/${currentWeather.iconCode}.png`;
 
@@ -105,38 +127,52 @@ searchForm.addEventListener('submit', async (e) => {
     hourlyDataContainer.classList.add('hourly-data');
 
     const weekday = document.createElement('p');
-    weekday.classList.add('weekday');
-    weekday.textContent = forecast[i].localDateTimeString;
+    weekday.classList.add('weekday-and-time');
+    weekday.textContent = `${forecast[i].localDateTimeString.substring(0, 4)} ${toTwelveHourTime(forecast[i].localDateTimeString.substring(16))}`;
 
     const hourlyTemp = document.createElement('p');
     hourlyTemp.classList.add('hourly-temperature');
-    hourlyTemp.textContent = forecast[i].temp;
+    hourlyTemp.textContent = `${Math.round(forecast[i].temp)} °F`;
 
     const hourlyFeelsLike = document.createElement('p');
     hourlyFeelsLike.classList.add('hourly-feels-like');
-    hourlyFeelsLike.textContent = forecast[i].feelsLike;
+    hourlyFeelsLike.textContent = `Feels Like ${Math.round(forecast[i].feelsLike)} °F`;
 
+    const precipIconAndDataContainer = document.createElement('div');
+    precipIconAndDataContainer.classList.add('hourly-precipitation-chance');
+    const raindropIcon = document.createElement('img');
+    raindropIcon.id = 'precipitation-icon';
+    raindropIcon.src = './Icons/probabilityOfPrecipitation.png';
     const precipProb = document.createElement('p');
-    precipProb.classList.add('hourly-preciptation-chance');
-    precipProb.textContent = forecast[i].probabilityOfPrecip;
+    precipProb.textContent = `${Math.round(forecast[i].probabilityOfPrecip * 100)} %`;
+    precipIconAndDataContainer.appendChild(raindropIcon);
+    precipIconAndDataContainer.appendChild(precipProb);
 
+    const hourlyDescriptionAndImage = document.createElement('div');
+    hourlyDescriptionAndImage.classList.add('hourly-description');
     const hourlyDescription = document.createElement('p');
-    hourlyDescription.classList.add('hourly-description');
     hourlyDescription.textContent = forecast[i].description;
-
     const hourlyImage = document.createElement('img');
     hourlyImage.src = `./Icons/${forecast[i].iconCode}.png`;
+    hourlyDescriptionAndImage.appendChild(hourlyDescription);
+    hourlyDescriptionAndImage.appendChild(hourlyImage);
 
     hourlyDataContainer.appendChild(weekday);
     hourlyDataContainer.appendChild(hourlyTemp);
     hourlyDataContainer.appendChild(hourlyFeelsLike);
-    hourlyDataContainer.appendChild(precipProb);
-    hourlyDataContainer.appendChild(hourlyDescription);
-    hourlyDataContainer.appendChild(hourlyImage);
+    hourlyDataContainer.appendChild(precipIconAndDataContainer);
+    hourlyDataContainer.appendChild(hourlyDescriptionAndImage);
     forecastContainer.appendChild(hourlyDataContainer);
   }
 
   searchForm.reset();
+}
+
+// display weather data on submitting city
+searchForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const searchValue = document.getElementById('search').value;
+  displayPage(searchValue);
 });
 
 // Remove error messsage when search input is changed
@@ -144,3 +180,5 @@ const searchInput = document.getElementById('search');
 searchInput.addEventListener('change', () => {
   document.querySelector('.error').style.display = 'none';
 });
+
+displayPage('denver');
